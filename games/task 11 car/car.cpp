bb46eaf3cfc25bcd46c0c0e6ba2e75e99a532bd5
Yahyakhan-creater <yahyahmadani801@gmail.com>
2026-02-28 02:13:59 +0500

    Add project files

---
+++ games/task 11 car/car.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+system("color 1F");
+
+cout<<endl;
+cout<<"                 _____________________________ "<<endl;
+cout<<"                |                             |"<<endl;
+cout<<"                |                             |"<<endl;
+cout<<"   _____________|_____________________________|________"<<endl;
+cout<<"  |                                                      |"<<endl;
+cout<<"  |                                                      |"<<endl;
+cout<<"  |                                                      |"<<endl;
+cout<<"  |______________________________________________________|"<<endl;
+cout<<"          O                                      O"<<endl;
+cout<<"        (___)                                  (___)"<<endl;
+cout<<endl;
+
+}
---
+++ games/task 5 op/header.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    system("color 2F");
+
+    cout << endl;
+    cout << " ##     ## ######## ######## " << endl;
+    cout << " ##     ## ##          ##    " << endl;
+    cout << " ##     ## ##          ##    " << endl;
+    cout << " ##     ## ######      ##    " << endl;
+    cout << " ##     ## ##          ##    " << endl;
+    cout << " ##     ## ##          ##    " << endl;
+    cout << "  #######  ########    ##    " << endl;
+
+    cout << endl;
+    cout << "              UET" << endl;
+    cout << endl;
+
+    return 0;
+}
